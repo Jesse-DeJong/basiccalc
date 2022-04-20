04fefee027bf568b4570a435408ece75f38f99f8
Jesse De Jong <jdoceau@gmail.com>
2022-04-20 12:36:48 +1000

    work

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# basiccalc
+# basiccalc:
+
+Calculator Project
+
+## HTML/SCSS
+Create a calculator to be rendered to the html page
+it should have number keys from 0 to 9
+It should have operator keys (+, -, /, *, =)
+It should have a display rendering the current calculation in a box at the top
+It should also have a “.” key
+You should choose a picture of a calculator from the Internet and reproduce it in HTML/SCSS
+## JavaScript
+Should render the current calculation in a box at the top (calculator display)
+It should handle decimals
+It doesn’t need to support orders of operation
+It should not use eval() or Function() constructor
+DEADLINE: Friday, 22nd of April (edited) 
---
+++ script.js
@@ -0,0 +1,21 @@
+// DOM Targeting
+const equals = document.getElementById("=");
+const plus = document.getElementById("+");
+const minus = document.getElementById("-");
+const divide = document.getElementById("/");
+const multiply = document.getElementById("*");
+const period = document.getElementById(".");
+
+const zero = document.getElementById("0");
+const one = document.getElementById("1");
+const two = document.getElementById("2");
+const three = document.getElementById("3");
+const four = document.getElementById("4");
+const five = document.getElementById("5");
+const six = document.getElementById("6");
+const seven = document.getElementById("7");
+const eight = document.getElementById("8");
+const nine = document.getElementById("9");
+
+const buttons = document.querySelectorAll("calc__button");
+
